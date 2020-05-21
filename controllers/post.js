1a2b12443492d0f3d8bccd04d2b48f66429eb21e
jamiesonreinhard <59634117+jamiesonreinhard@users.noreply.github.com>
2020-05-21 13:38:05 -0600

    added postedBy functionality

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,6 @@
-const Post = require("../models/post")
-
+const Post = require("../models/post");
+const formidable = require('formidable');
+const fs = require('fs');
 
 exports.getPosts = (req, res) => {
     const posts = Post.find().select("_id title body")
@@ -9,19 +10,30 @@ exports.getPosts = (req, res) => {
     .catch(err => console.log(err));
 };
 
-exports.createPost = (req, res) => {
-    const post = new Post(req.body)
-    // // console.log("CREATING POST: ", post);  
-    // post.save((err, result) => {
-    //         res.status(200).json({
-    //             post: result
-    //         });
-    //     }
-    // });
-    post.save()
-    .then(result => {
-        res.status(200).json({
-            post: result
-        });
+exports.createPost = (req, res, next) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: "Image could not be uploaded."
+            })
+        }
+        let post = new Post(fields)
+        req.profile.hashed_password = undefined
+        req.profile.salt = undefined
+        post.postedBy = req.profile
+        if(files.photo) {
+            post.photo.data = fs.readFileSync(files.photo.path)
+            post.photo.contentType = files.photo.type
+        }
+        post.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: err
+                })
+            }
+            res.json(result)
+        })
     });
 };
